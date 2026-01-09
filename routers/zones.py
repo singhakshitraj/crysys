8c2e94658ef080f9efa7e65535322cefa627ad66
Akshit Raj Singh <akshitraj.rkt@gmail.com>
2026-01-09 17:55:49 +0530

    createDisasterZone endpoint created

--- routers/zones.py
+++ routers/zones.py
@@ -1,33 +1,48 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,status
 from psycopg2.extensions import connection
 from psycopg2.extras import  RealDictCursor
-from utils.validation_models.disaster_zone import DisasterZoneDto
+from utils.validation_models.disaster_zone import DisasterZoneDto,CreateDisasterZoneDto
 from config.db import db_connect
 
 router=APIRouter()
-def find_all_disaster_zones(conn: connection):
+
+@router.get('/zones')
+def summary(conn=Depends(db_connect)):
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         cur.execute("""
-            SELECT
-                id,
-                name,
-                disaster_type,
-                danger_level,
-                is_active,
-                created_at
+            SELECT *
             FROM disaster_zone
             ORDER BY created_at DESC
         """)
         return cur.fetchall()
 
-def get_all_disaster_zones(conn:connection) -> list[DisasterZoneDto]:
-    try:
-        rows = find_all_disaster_zones(conn)
-        return [DisasterZoneDto(**row) for row in rows]
 
-    finally:
-        conn.close()
-
-@router.get('/zones',response_model=list[DisasterZoneDto])
-def summary(conn=Depends(db_connect)):
-    return get_all_disaster_zones(conn)
+@router.post("/admin/zones",status_code=status.HTTP_201_CREATED)
+def create_disaster_zone(req: CreateDisasterZoneDto,db: connection = Depends(db_connect)):
+    print(req)
+    print(str(req.disaster_type.value))
+    query = """
+        INSERT INTO disaster_zone
+        (name, disaster_type, danger_level, latitude, longitude, radius)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        RETURNING id, name, disaster_type, danger_level,
+                  latitude, longitude, radius;
+    """
+    with db.cursor() as cursor:
+        cursor.execute(
+            query,
+            (
+                req.name,
+                req.disaster_type.value,
+                req.danger_level.value,
+                req.center_latitude,
+                req.center_longitude,
+                req.radius,
+            )
+        )
+        row = cursor.fetchone()
+        db.commit()
+    return {
+        'status':'201',
+        'data':row
+    }
--- utils/validation_models/disaster_zone.py
+++ utils/validation_models/disaster_zone.py
@@ -10,3 +10,19 @@ class DisasterZoneDto(BaseModel):
     danger_level: DangerLevel
     is_active: bool | None
     created_at: datetime
+
+class CreateDisasterZoneRequest(BaseModel):
+    name: str
+    disaster_type: DisasterType
+    danger_level: DangerLevel
+    center_latitude: float
+    center_longitude: float
+    radius: float
+    
+class CreateDisasterZoneDto(BaseModel):
+    name: str
+    disaster_type: DisasterType
+    danger_level: DangerLevel
+    center_latitude: float
+    center_longitude: float
+    radius: float
